61f8dc4bca4be795f4422efaea4eee3a599ce782
RVKPP-Lakmina <rvkpplakmina2000@gmail.com>
2026-04-19 22:41:55 +0530

    feat(issues): expose project and time-entry routes

--- src/modules/issues/issues.routes.ts
+++ src/modules/issues/issues.routes.ts
@@ -2,15 +2,27 @@ import { Router } from "express";
 import { authGuard } from "../../middlewares/authGuard";
 import { validate } from "../../middlewares/validate";
 import {
+    createProjectController,
+    createTimeEntryController,
     createIssueController,
     deleteIssueController,
+    exportTimeEntriesCsvController,
+    listProjectsController,
+    listTimeEntriesController,
     listIssuesController,
+    updateProjectController,
     updateIssueController
 } from "./issues.controller";
 import {
+    createProjectSchema,
+    createTimeEntrySchema,
     createIssueSchema,
     issueIdParamsSchema,
+    listProjectsSchema,
+    listTimeEntriesSchema,
     listIssuesSchema,
+    timeEntriesReportSchema,
+    updateProjectSchema,
     updateIssueSchema
 } from "./issues.validation";
 
@@ -20,5 +32,18 @@ issuesRouter.get("/", authGuard, validate(listIssuesSchema), listIssuesControlle
 issuesRouter.post("/", authGuard, validate(createIssueSchema), createIssueController);
 issuesRouter.put("/:id", authGuard, validate(updateIssueSchema), updateIssueController);
 issuesRouter.delete("/:id", authGuard, validate(issueIdParamsSchema), deleteIssueController);
+
+issuesRouter.get("/projects", authGuard, validate(listProjectsSchema), listProjectsController);
+issuesRouter.post("/projects", authGuard, validate(createProjectSchema), createProjectController);
+issuesRouter.put("/projects/:id", authGuard, validate(updateProjectSchema), updateProjectController);
+
+issuesRouter.get("/time-entries", authGuard, validate(listTimeEntriesSchema), listTimeEntriesController);
+issuesRouter.post("/time-entries", authGuard, validate(createTimeEntrySchema), createTimeEntryController);
+issuesRouter.get(
+    "/time-entries/report.csv",
+    authGuard,
+    validate(timeEntriesReportSchema),
+    exportTimeEntriesCsvController
+);
 
 export { issuesRouter };
