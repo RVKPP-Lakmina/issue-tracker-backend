4d2e1e9746694b1c454fa4cba4fd6f936b2980a1
RVKPP-Lakmina <rvkpplakmina2000@gmail.com>
2026-04-20 10:13:23 +0530

    feat(auth): add refresh route and update logout route

--- src/modules/auth/auth.routes.ts
+++ src/modules/auth/auth.routes.ts
@@ -1,14 +1,15 @@
 import { Router } from "express";
 import { authGuard } from "../../middlewares/authGuard";
 import { validate } from "../../middlewares/validate";
-import { logoutController, meController, signInController, signUpController } from "./auth.controller";
+import { logoutController, meController, refreshController, signInController, signUpController } from "./auth.controller";
 import { signInSchema, signUpSchema } from "./auth.validation";
 
 const authRouter = Router();
 
 authRouter.post("/signup", validate(signUpSchema), signUpController);
 authRouter.post("/signin", validate(signInSchema), signInController);
-authRouter.post("/logout", authGuard, logoutController);
+authRouter.post("/refresh", refreshController);
+authRouter.post("/logout", logoutController);
 authRouter.get("/me", authGuard, meController);
 
 export { authRouter };
